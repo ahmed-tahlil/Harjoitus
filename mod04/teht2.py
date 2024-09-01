f27ed2f93acdb4abe3ffccfa7cad9304696b392f
ahmedta <ahmedta@metropolia.fi>
2024-09-01 17:58:55 +0300

    initial commit

---
+++ mod04/teht2.py
@@ -0,0 +1,10 @@
+tuuma = input("Kerro tuumien määrä: ")
+tuuma = int(tuuma)
+while tuuma >= 0:
+    sentti = tuuma * 2.54
+    print(f"Antamasi tuumat {tuuma} ovat {sentti} senttimetriä.")
+    tuuma = int(input("Kerro tuumien määrä: "))
+    if tuuma < 0:
+        break
+
+##print(tuuma)
