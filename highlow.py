144215f8bc03d38c4ccf360f28da1f50bbe8e93c
ahmed-tahlil <ahmedta@metropolia.fi>
2024-09-29 17:10:15 +0300

    Update highlow.py

--- highlow.py
+++ highlow.py
@@ -9,7 +9,7 @@ def highlow():
 
     korttipakka = [(numero, maa) for maa in maat for numero in numerot]
 
-    # Muunna korttien arvot numeroksi vertailua varten
+   
     korttiarvot = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
                    'J': 11, 'Q': 12, 'K': 13, 'A': 14}
 
@@ -30,7 +30,7 @@ def highlow():
     # pelin funktio
 
     def peli_hl():
-        ### pelin toiminnot
+       
         random.shuffle(korttipakka)
         pisteet = 0
         loss = 0
