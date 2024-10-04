89701352271d2989d0fbc98637afe01ec70b493d
ahmedta <ahmedta@metropolia.fi>
2024-10-04 18:16:34 +0300

    initial commit

--- mod08/travel.py
+++ mod08/travel.py
@@ -44,10 +44,52 @@ def get_closest_airports(p_id, airports):
 
     return closest_airports
 
+def closest_airport(closest_airports):
+    min_airport = closest_airports[0][0]
+    min_dist = closest_airports[0][1]
+
+    for airport, dist in closest_airports:
+        if dist < min_dist:
+            min_airport = airport
+            min_dist = dist
+
+    return min_airport
+
 
 
 
 def travel(airports, money, co2, day):
     available_airports = check_airport_co2_level(airports, co2)
+    if not available_airports:
+        print("Cannot travel, do environment task")
+        return environmental_task(money,co2,day)
     closest_airports = get_closest_airports(available_airports)
+
+    travel_methods = {
+        "1": {"method": "airplane", "money": 1000, "co2": 1000, "days": 1},
+        "2": {"method": "train", "money": 500, "co2": 250, "days": 3},
+        "3": {"method": "bicycle", "money": 100, "co2": 0, "days": 5}
+    }
+
+    print("Choose how you want to travel: ")
+    print("1. Airplane (Cost: 1000€, CO2: 1000, Days: 1)")
+    print("2. Train (Cost: 500€, CO2: 250, Days: 3)")
+    print("3. Bicycle (Cost: 100€, CO2: 0, Days: 5)")
+
+    method = input("How do you want to travel?")
+
+    if method in travel_methods:
+        travel_method = travel_methods[method]
+        money = money - travel_method["money"]
+        co2 = co2 - travel_method["co2"]
+        day = day - travel_method["days"]
+
+    else:
+        print("Invalid input. Try again.")
+        return travel(airports, money, co2, day)
+
+    return money,co2,day
+
+
+
 
